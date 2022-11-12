785e1bcf3f351ff4b84d3120269f21a26a82b6a2
Argueta Lab <81782750+ArguetaLab@users.noreply.github.com>
2022-11-12 10:15:54 -0600

    Create Nivel_Zelda.py

---
+++ Nivel_Zelda.py
@@ -0,0 +1,27 @@
+import pygame
+import Mapa_Zelda
+from Tiles_Zelda import Tile
+from Link import Link
+
+
+class Nivel:
+    def __init__(self):
+        self.screen = pygame.display.get_surface()
+        self.Sprites_deFondo = pygame.sprite.Group()
+        self.Obtaculos = pygame.sprite.Group()
+
+        self.crearMapa()
+
+    def crearMapa(self):
+        for row_index, row in enumerate(Mapa_Zelda.mapa):
+            for col_index, col in enumerate(row):
+                x = col_index * 64
+                y = row_index * 56
+                if col == "x":
+                    Tile((x,y), [self.Sprites_deFondo, self.Obtaculos])
+                if col == "L":
+                    Link((x,y), [self.Sprites_deFondo])
+
+    def corre(self):
+        self.Sprites_deFondo.draw(self.screen)
+        self.Sprites_deFondo.update()
